ad8363fca6fc04fcf050ffe003fbf660a23848c0
Roshan Nasseri <roshan.sep26@gmail.com>
2021-06-04 16:50:29 +0000

    fix schema timestamp default values

--- backend/db/schema/01_users.sql
+++ backend/db/schema/01_users.sql
@@ -9,14 +9,14 @@ CREATE TABLE "users" (
   "full_name" VARCHAR(255) NOT NULL,
   "email" VARCHAR(255) NOT NULL,
   "password" VARCHAR(255) NOT NULL,
-  "created_at" TIMESTAMP NOT NULL
+  "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE "projects" (
   "id" SERIAL PRIMARY KEY NOT NULL,
   "name" VARCHAR(255) NOT NULL,
   "description" TEXT, 
-  "created_at" TIMESTAMP NOT NULL
+  "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE "users_projects" (
@@ -24,7 +24,7 @@ CREATE TABLE "users_projects" (
   "project_id" INTEGER REFERENCES projects(id) ON DELETE CASCADE,
   "user_id" INTEGER REFERENCES users(id) ON DELETE CASCADE,
   "owner_stretch" BOOLEAN DEFAULT false,
-  "created_at" TIMESTAMP NOT NULL
+  "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE "files" (
@@ -33,13 +33,13 @@ CREATE TABLE "files" (
   "name" VARCHAR(255) NOT NULL,
   "location" VARCHAR(255) NOT NULL,
   "description" text,
-  "created_at" TIMESTAMP NOT NULL
+  "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE "comments_stretch" (
   "id" SERIAL PRIMARY KEY NOT NULL,
   "user_id" INTEGER REFERENCES users(id) ON DELETE CASCADE,
   "file_id" INTEGER REFERENCES files(id) ON DELETE CASCADE,
-  "created_at" TIMESTAMP NOT NULL
+  "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
