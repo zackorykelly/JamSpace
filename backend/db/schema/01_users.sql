956363eabd1c4a456d54ce5ff3ca97d4a51f7f41
Roshan Nasseri <roshan.sep26@gmail.com>
2021-06-04 16:20:01 +0000

    create database schema

---
+++ backend/db/schema/01_users.sql
@@ -0,0 +1,45 @@
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS projects CASCADE;
+DROP TABLE IF EXISTS users_projects CASCADE;
+DROP TABLE IF EXISTS files CASCADE;
+DROP TABLE IF EXISTS comments_stretch CASCADE;
+
+CREATE TABLE "users" (
+  "id" SERIAL PRIMARY KEY NOT NULL,
+  "full_name" VARCHAR(255) NOT NULL,
+  "email" VARCHAR(255) NOT NULL,
+  "password" VARCHAR(255) NOT NULL,
+  "created_at" TIMESTAMP NOT NULL
+);
+
+CREATE TABLE "projects" (
+  "id" SERIAL PRIMARY KEY NOT NULL,
+  "name" VARCHAR(255) NOT NULL,
+  "description" TEXT, 
+  "created_at" TIMESTAMP NOT NULL
+);
+
+CREATE TABLE "users_projects" (
+  "id" SERIAL PRIMARY KEY NOT NULL,
+  "project_id" INTEGER REFERENCES projects(id) ON DELETE CASCADE,
+  "user_id" INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  "owner_stretch" BOOLEAN DEFAULT false,
+  "created_at" TIMESTAMP NOT NULL
+);
+
+CREATE TABLE "files" (
+  "id" SERIAL PRIMARY KEY NOT NULL,
+  "project_id" INTEGER REFERENCES projects(id) ON DELETE CASCADE,
+  "name" VARCHAR(255) NOT NULL,
+  "location" VARCHAR(255) NOT NULL,
+  "description" text,
+  "created_at" TIMESTAMP NOT NULL
+);
+
+CREATE TABLE "comments_stretch" (
+  "id" SERIAL PRIMARY KEY NOT NULL,
+  "user_id" INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  "file_id" INTEGER REFERENCES files(id) ON DELETE CASCADE,
+  "created_at" TIMESTAMP NOT NULL
+);
+
