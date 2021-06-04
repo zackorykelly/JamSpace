965bfff09e235f80838dc0fc486a18129942ac26
Roshan Nasseri <roshan.sep26@gmail.com>
2021-06-04 16:51:07 +0000

    add half of the seeds for populating the db

---
+++ backend/db/seeds/01_users.sql
@@ -0,0 +1,28 @@
+INSERT INTO users (full_name, email, password) VALUES
+('Brooklynn Perez', 'brooklynnp@gmail.com', 'password' ),
+('Duncan Wilcox', 'duncanw@gmail.com', 'password'),
+('Malik Rios', 'malikr@gmail.com', 'password'),
+('Miguel Stewart', 'miguels@gmail.com', 'password'),
+('Corinne Phillips', 'corinnep@gmail.com', 'password'),
+('Greyson Carroll', 'greysonc@gmail.com', 'password'),
+('Madison Smith', 'madisons@gmail.com', 'password'),
+('Hamza Richardson', 'hamzar@gmail.com', 'password'),
+('Corbin Gillespie', 'corbing@gmail.com', 'password'),
+('Miah Gentry', 'miahg@gmail.com', 'password'),
+('Nylah Hall', 'nylahh@gmail.com', 'password'),
+('Hayley Sanders', 'hayleys@gmail.com', 'password'),
+('Sawyer Shepard', 'sawyers@gmail.com', 'password'),
+('Elise Fowler', 'elisef@gmail.com', 'password');
+
+INSERT INTO projects (name, description, created_at) VALUES
+('Simple riffs', 'A development project just to get some of my ideas that come to me out there so we can mash them up', '2020-12-16'::timestamp),
+('Mega ideas', 'I like piano, you like guitar lets whip smt up for the gig', '2020-12-20'::timestamp),
+('GH highschool project', 'Hello classmates record and submit your records to this project', '2020-12-24'::timestamp),
+('Band ideas', 'composition ideas for the London marching band', '2021-01-02'::timestamp),
+("Duncan's project", 'My simple ideas', '2021-01-12'::timestamp),
+('Potato room', 'This is for all the boy and girls out there who like music and french fries', '2021-01-16'::timestamp),
+('Dollar Bling', 'These are the ideas of where money is made', '2021-01-30'::timestamp),
+('Aqua 6', 'We got the main meoldy down try and add some beats to the melody and see what we can come up with for the North American tour', '2021-02-15'::timestamp),
+('The blues', 'Just a simple project to see what we can mash ontop of a chill blues', '2021-03-13'::timestamp),
+('Jaz comp', 'just playin around with some swing', '2021-03-21'::timestamp),
+('Purdie Shuffle', 'My final piece for the grand hall preformance', '2020-04-19'::timestamp),
