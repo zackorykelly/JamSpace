bc30233092feb31a67d5520e280157f383f5da46
chinny36 <brownj95@gmail.com>
2021-06-05 13:21:03 -0400

    Get users from backend

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,62 +1,56 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {
-    getPostsByUsers
-} = require('../helpers/dataHelpers');
+const { getPostsByUsers } = require("../helpers/dataHelpers");
 
-module.exports = ({
-    getUsers,
-    getUserByEmail,
-    addUser,
-    getUsersPosts
-}) => {
-    /* GET users listing. */
-    router.get('/', (req, res) => {
-        getUsers()
-            .then((users) => res.json(users))
-            .catch((err) => res.json({
-                error: err.message
-            }));
-    });
+module.exports = ({ getUsers, getUserByEmail, addUser, getUsersPosts }) => {
+  /* GET users listing. */
+  router.get("/", (req, res) => {
+    console.log("calling api");
+    getUsers()
+      .then((users) => {
+        console.log(users);
+        res.json(users);
+      })
+      .catch((err) =>
+        res.json({
+          error: err.message
+        })
+      );
+  });
 
-    router.get('/posts', (req, res) => {
-        getUsersPosts()
-            .then((usersPosts) => {
-                const formattedPosts = getPostsByUsers(usersPosts);
-                res.json(formattedPosts);
-            })
-            .catch((err) => res.json({
-                error: err.message
-            }));
-    });
+  router.get("/posts", (req, res) => {
+    getUsersPosts()
+      .then((usersPosts) => {
+        const formattedPosts = getPostsByUsers(usersPosts);
+        res.json(formattedPosts);
+      })
+      .catch((err) =>
+        res.json({
+          error: err.message
+        })
+      );
+  });
 
-    router.post('/', (req, res) => {
+  router.post("/", (req, res) => {
+    const { first_name, last_name, email, password } = req.body;
 
-        const {
-            first_name,
-            last_name,
-            email,
-            password
-        } = req.body;
+    getUserByEmail(email)
+      .then((user) => {
+        if (user) {
+          res.json({
+            msg: "Sorry, a user account with this email already exists"
+          });
+        } else {
+          return addUser(first_name, last_name, email, password);
+        }
+      })
+      .then((newUser) => res.json(newUser))
+      .catch((err) =>
+        res.json({
+          error: err.message
+        })
+      );
+  });
 
-        getUserByEmail(email)
-            .then(user => {
-
-                if (user) {
-                    res.json({
-                        msg: 'Sorry, a user account with this email already exists'
-                    });
-                } else {
-                    return addUser(first_name, last_name, email, password)
-                }
-
-            })
-            .then(newUser => res.json(newUser))
-            .catch(err => res.json({
-                error: err.message
-            }));
-
-    })
-
-    return router;
+  return router;
 };
