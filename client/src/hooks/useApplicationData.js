07cebf3bb45c53bc3ade7e10545af8daeac5436b
chinny36 <brownj95@gmail.com>
2021-06-04 21:00:42 -0400

    Add CSS for project list item and src/styles folder

--- client/src/hooks/useApplicationData.js
+++ client/src/hooks/useApplicationData.js
@@ -1,38 +1,29 @@
-import {
-  useEffect,
-  useReducer
-} from 'react';
-import dataReducer, {
-  SET_USERS
-} from '../reducer/data_reducer';
-import axios from 'axios';
+import { useEffect, useReducer } from "react";
+import dataReducer, { SET_USERS } from "../reducer/data_reducer";
+import axios from "axios";
 
-const useApplicationData = () => {
+export default function useApplicationData() {
   const [state, dispatch] = useReducer(dataReducer, {
-      users: [],
-      loading: true,
+    users: [],
+    loading: true
   });
   useEffect(() => {
-      axios({
-              method: 'GET',
-              url: '/api/users',
-          })
-          .then(({
-              data
-          }) => {
-              console.log(data);
-              dispatch({
-                  type: SET_USERS,
-                  users: data
-              });
-          })
-          .catch((err) => console.log(err));
+    axios({
+      method: "GET",
+      url: "/api/users"
+    })
+      .then(({ data }) => {
+        console.log(data);
+        dispatch({
+          type: SET_USERS,
+          users: data
+        });
+      })
+      .catch((err) => console.log(err));
   }, []);
 
   return {
-      state,
-      dispatch,
+    state,
+    dispatch
   };
-};
-
-export default useApplicationData;
+}
