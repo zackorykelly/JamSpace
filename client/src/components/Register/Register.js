762138b2f48199a5a700f07e177ed24e67e1dd2b
roshan0926 <80285716+roshan0926@users.noreply.github.com>
2021-06-07 19:37:44 +0100

    clean up code

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -4,19 +4,6 @@ import { useHistory } from "react-router-dom";
 import "./Register.scss";
 
 export default function Login(props) {
-  // const [inputs, setInputs] = useState({
-  //   name: "",
-  //   email: "",
-  //   password: ""
-  // })
-  // const { name, email, password } = inputs
-  // const onChange =(e) => {
-  //   console.log("onChange ", e.target.value)
-  //   setInputs({...inputs, [e.target.name]
-  //    : e.target.value});
-  // };
-  // const {name = inputs
-
   let history = useHistory();
 
   const { register, handleSubmit, watch, formState: { errors } } = useForm();
@@ -24,12 +11,11 @@ export default function Login(props) {
     console.log('data', data)
     fetch("/api/users", {
       method: "POST",
-      headers: {'Content-Type': 'application/json'},
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(data)
     })
-    .then((res) => console.log(res))
-    .catch((error) => console.error(error.message))
-
+      .then((res) => console.log(res))
+      .catch((error) => console.error(error.message))
     history.push("/")
   }
 
@@ -38,43 +24,19 @@ export default function Login(props) {
     <div className="login-form">
       <form onSubmit={handleSubmit(onSubmit)}>
         <div id="float-full-name">
-          <input
-            {...register("name")}
-            type="text"
-
-
-          />
-          <label className="Active">
-            Full Name
-  </label>
+          <input {...register("name")} type="text" />
+          <label className="Active">Full Name</label>
         </div>
-
         <div id="float-email">
-          <input
-            {...register("email")}
-            type="email"
-
-          />
-
-          <label className="Active">
-            E-mail
-  </label>
+          <input {...register("email")} type="email" />
+          <label className="Active">E-mail</label>
         </div>
-
         <div id="float-password">
-          <input
-            {...register("password")}
-            type="password"
-
-          />
-
-          <label className="Active">
-            Password
-  </label>
+          <input {...register("password")} type="password" />
+          <label className="Active">Password</label>
         </div>
         <input type="submit" className="login-button" />
       </form>
     </div>
-  </>
-    ;
-  }
+  </>;
+}
