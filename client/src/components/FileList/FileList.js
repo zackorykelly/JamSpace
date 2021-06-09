2a0117756c823ffd204339e0d8859829378f0be3
chinny36 <brownj95@gmail.com>
2021-06-09 10:14:06 -0400

    Comments

--- client/src/components/FileList/FileList.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import FileListItem from "../FileListItem/FileListItem";
-import "./FileList.scss";
-
-export default function FileList(props) {
-  return <h1>I am File List</h1>;
-}
--- client/src/components/Project/Project.js
+++ client/src/components/Project/Project.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 export default function Project(props) {
-  const listFiles = props.files.map((file) => {
-    <p>{JSON.stringify(file)}</p>;
-  });
+  // const listFiles = props.files.map((file) => {
+
+  // });
 
   return (
     <>
       <h1>Project: {props.project.name}</h1>
-      {listFiles}
+      {/* {listFiles} */}
       <button onClick={props.closeProject}>Close</button>
     </>
   );
