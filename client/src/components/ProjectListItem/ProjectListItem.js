260dcb8bbd7a6b9f7e7e07c966a720553f8061d1
chinny36 <brownj95@gmail.com>
2021-06-13 17:11:38 -0400

    merge bug fixes and update CSS

--- client/src/components/ProjectListItem/ProjectListItem.js
+++ client/src/components/ProjectListItem/ProjectListItem.js
@@ -2,6 +2,7 @@ import React from "react";
 import classNames from "classnames";
 import "./ProjectListItem.scss";
 import { DELETE_PROJECT } from "../../reducer/data_reducer";
+import { IoCloseCircleOutline } from "react-icons/io5";
 
 export default function ProjectListItem(props) {
   let projectClass = classNames("project-list__item");
@@ -37,10 +38,16 @@ export default function ProjectListItem(props) {
           props.setProject(props.projectId);
         }}
       >
-        <h4>{props.name}: </h4>
-        <p>{props.description}</p>
-        <button type="button" onClick={(e) => deleteProject(e)}>
-          Delete
+        <p>
+          <h5>{props.name}: </h5>
+          {props.description}
+        </p>
+        <button
+          type="button"
+          className="close btn"
+          onClick={(e) => deleteProject(e)}
+        >
+          <IoCloseCircleOutline size={25} />
         </button>
       </section>
     </>
